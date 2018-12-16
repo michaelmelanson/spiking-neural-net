09407a67c0179ced200dfd30c59a7b3773ebd5cb
Michael Melanson <michael@michaelmelanson.net>
2018-12-16 11:21:10 -0500

    Remove unnecessary 'extern crate' statements

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-
-extern crate num;
 #[macro_use] extern crate specs_derive;
-extern crate specs;
-extern crate rayon;
 #[macro_use] extern crate log;
-extern crate pretty_env_logger;
-extern crate rand;
-extern crate serde;
 #[macro_use] extern crate serde_derive;
-extern crate serde_yaml;
-extern crate core;
 
 mod simulation;
 
